9b5d711f2b4cce04af2129b2c4256caf525deddf
sivaskt <sivaskt23@gmail.com>
2024-07-23 18:29:30 +0530

    Adding files

---
+++ src/Manchodu.java
@@ -0,0 +1,13 @@
+
+public class Manchodu 
+{
+
+	public static void main(String[] args)
+	{
+      System.out.println(new Manchodu().sarayu());
+	}
+     String sarayu()
+     {
+    	 return "sarayu";
+     }
+}
