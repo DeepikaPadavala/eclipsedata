2975cc949e105c2481c485c59ae600937f0005f2
sivaskt <sivaskt23@gmail.com>
2024-07-23 19:09:59 +0530

    updating file

--- src/Alochinchandi.java
+++ src/Alochinchandi.java
@@ -1,9 +1,14 @@
 
 public class Alochinchandi {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		// TODO Auto-generated method stub
 
 	}
-
+    void endhukuekkadavunam ()
+    {
+    	
+    }
 }
+    
